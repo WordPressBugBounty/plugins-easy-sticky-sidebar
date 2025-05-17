b3df317da0176b40c0845727444c57aca14f3dba
Sean Murphy <sean@iamseanmurphy.com>
2025-05-17 00:37:33 +0000

    1.6.6

--- easy-sticky-sidebar/inc/sticky-cta-data.php
+++ easy-sticky-sidebar/inc/sticky-cta-data.php
@@ -8,97 +8,102 @@
 class WP_Sticky_CTA_Data {
     private $sticky_data = null;
     
-    // Declare all standard properties
-    public $id = 0;
-    public $SSuprydp_impressions = "0";
-    public $SSuprydp_clicks = "0";
-    public $SSuprydp_development = "development";
-    public $SSuprydp_shrink = "No";
-    public $SSuprydp_shrink_tablet = "No";
-    public $SSuprydp_shrink_mobile = "No";
-    public $SSuprydp_dis_desktop = "Yes";
-    public $SSuprydp_dis_tablet = "Yes";
-    public $SSuprydp_dis_mobile = "Yes";
-    public $SSuprydp_location = "home";
-    public $SSuprydp_location_type = "Pages";
-    public $SSuprydp_img_hideimg = "No";
-    public $SSuprydp_hideimg_tablet = "No";
-    public $SSuprydp_hideimg_mobile = "No";
-    public $sticky_s_media = "";
-    public $image_attachment_id = "0";
-    public $SSuprydp_button_option_text = "Click Here";
-    public $SSuprydp_button_option_backg_color = "#4e0d61";
-    public $SSuprydp_button_option_font = "Open Sans";
-    public $SSuprydp_button_option_weight = "400";
-    public $SSuprydp_button_option_size = "20";
-    public $SSuprydp_button_option_align = "left";
-    public $SSuprydp_button_option_color = "#fff";
-    public $SSuprydp_content_option_text = "This is the Content Area. Put a description here of what you want to promote.";
-    public $SSuprydp_content_option_font = "Open Sans";
-    public $SSuprydp_content_option_weight = "800";
-    public $SSuprydp_content_option_size = "25";
-    public $SSuprydp_content_option_color = "#fff";
-    public $SSuprydp_divider_option_color = "#1b7ccc";
-    public $SSuprydp_action_option_text = "Click Here to View";
-    public $SSuprydp_action_option_font = "Open Sans";
-    public $SSuprydp_action_option_weight = "500";
-    public $SSuprydp_action_option_size = "19";
-    public $SSuprydp_action_option_color = "#fff";
-    public $SSuprydp_action_option_url = "https://wpctapro.com/";
-    public $SSuprydp_target_blank = "No";
-    public $SSuprydp_nofollow = "No";
-    public $SSuprydp_cta_position = "center";
-    public $sidebar_template = "sticky-cta";
-    public $line_separator_show = "yes";
-    public $line_separator_color = "#fff";
-    public $collapse_on_page_load = "no";
-    public $locations = array();
-    public $exclude_locations = array();
+    // Main class properties
+    protected $id = 0;
+    protected $SSuprydp_impressions = "0";
+    protected $SSuprydp_clicks = "0";
+    protected $SSuprydp_development = "development";
+    protected $SSuprydp_shrink = "No";
+    protected $SSuprydp_shrink_tablet = "No";
+    protected $SSuprydp_shrink_mobile = "No";
+    protected $SSuprydp_dis_desktop = "Yes";
+    protected $SSuprydp_dis_tablet = "Yes";
+    protected $SSuprydp_dis_mobile = "Yes";
+    protected $SSuprydp_location = "home";
+    protected $SSuprydp_location_type = "Pages";
+    protected $SSuprydp_img_hideimg = "No";
+    protected $SSuprydp_hideimg_tablet = "No";
+    protected $SSuprydp_hideimg_mobile = "No";
+    protected $sticky_s_media = "";
+    protected $image_attachment_id = "0";
+    protected $SSuprydp_button_option_text = "Click Here";
+    protected $SSuprydp_button_option_backg_color = "#4e0d61";
+    protected $SSuprydp_button_option_font = "Open Sans";
+    protected $SSuprydp_button_option_weight = "400";
+    protected $SSuprydp_button_option_size = "20";
+    protected $SSuprydp_button_option_align = "left";
+    protected $SSuprydp_button_option_color = "#fff";
+    protected $SSuprydp_content_option_text = "";
+    protected $SSuprydp_content_option_font = "Open Sans";
+    protected $SSuprydp_content_option_weight = "800";
+    protected $SSuprydp_content_option_size = "25";
+    protected $SSuprydp_content_option_color = "#fff";
+    protected $SSuprydp_divider_option_color = "#1b7ccc";
+    protected $SSuprydp_action_option_text = "Click Here to View";
+    protected $SSuprydp_action_option_font = "Open Sans";
+    protected $SSuprydp_action_option_weight = "500";
+    protected $SSuprydp_action_option_size = "19";
+    protected $SSuprydp_action_option_color = "#fff";
+    protected $SSuprydp_action_option_url = "https://wpctapro.com/";
+    protected $SSuprydp_target_blank = "No";
+    protected $SSuprydp_nofollow = "No";
+    protected $SSuprydp_cta_position = "center";
+    protected $sidebar_template = "sticky-cta";
+    protected $line_separator_show = "yes";
+    protected $line_separator_color = "#fff";
+    protected $collapse_on_page_load = "no";
     
-    // For any dynamic properties that might come from the database
-    private $dynamic_properties = array();
+    // Properties for locations
+    protected $locations = [];
+    protected $exclude_locations = [];
+    
+    // Property to store post-related data
+    protected $post_data = [];
+    
+    // Store all dynamic properties in this array
+    protected $dynamic_properties = [];
 
-    function __construct($sticky_data = array()) {
-        $this->sticky_data = (object) wp_parse_args($sticky_data, apply_filters('wordpress_sticky_cta_defaults', array(
+    function __construct($sticky_data = []) {
+        $this->sticky_data = (object) wp_parse_args($sticky_data, apply_filters( 'wordpress_sticky_cta_defaults', array(
             'id' => 0,
-            "SSuprydp_impressions" => "0",
-            "SSuprydp_clicks" => "0",
-            "SSuprydp_development" => "development",
-            "SSuprydp_shrink" => "No",
-            "SSuprydp_shrink_tablet" => "No",
-            "SSuprydp_shrink_mobile" => "No",
-            "SSuprydp_dis_desktop" => "Yes",
-            "SSuprydp_dis_tablet" => "Yes",
-            "SSuprydp_dis_mobile" => "Yes",
-            "SSuprydp_location" => "home",
-            "SSuprydp_location_type" => "Pages",
-            "SSuprydp_img_hideimg" => "No",
-            "SSuprydp_hideimg_tablet" => "No",
-            "SSuprydp_hideimg_mobile" => "No",
-            "sticky_s_media" => "",
-            "image_attachment_id" => "0",
-            "SSuprydp_button_option_text" => "Click Here",
-            "SSuprydp_button_option_backg_color" => "#4e0d61",
-            "SSuprydp_button_option_font" => "Open Sans",
-            "SSuprydp_button_option_weight" => "400",
-            "SSuprydp_button_option_size" => "20",
-            "SSuprydp_button_option_align" => "left",
-            "SSuprydp_button_option_color" => "#fff",
-            "SSuprydp_content_option_text" => "This is the Content Area. Put a description here of what you want to promote.",
-            "SSuprydp_content_option_font" => "Open Sans",
-            "SSuprydp_content_option_weight" => "800",
-            "SSuprydp_content_option_size" => "25",
-            "SSuprydp_content_option_color" => "#fff",
-            "SSuprydp_divider_option_color" => "#1b7ccc",
-            "SSuprydp_action_option_text" => "Click Here to View",
-            "SSuprydp_action_option_font" => "Open Sans",
-            "SSuprydp_action_option_weight" => "500",
-            "SSuprydp_action_option_size" => "19",
-            "SSuprydp_action_option_color" => "#fff",
-            "SSuprydp_action_option_url" => "https://wpctapro.com/",
-            "SSuprydp_target_blank" => "No",
-            "SSuprydp_nofollow" => "No",
-            "SSuprydp_cta_position" => "center",
+            "SSuprydp_impressions"=>"0",
+            "SSuprydp_clicks"=>"0",
+            "SSuprydp_development"=>"development",
+            "SSuprydp_shrink"=>"No",
+            "SSuprydp_shrink_tablet"=>"No",
+            "SSuprydp_shrink_mobile"=>"No",
+            "SSuprydp_dis_desktop"=>"Yes",
+            "SSuprydp_dis_tablet"=>"Yes",
+            "SSuprydp_dis_mobile"=>"Yes",
+            "SSuprydp_location"=>"home",
+            "SSuprydp_location_type"=>"Pages",
+            "SSuprydp_img_hideimg"=>"No",
+            "SSuprydp_hideimg_tablet"=>"No",
+            "SSuprydp_hideimg_mobile"=>"No",
+            "sticky_s_media"=>"",
+            "image_attachment_id"=>"0",
+            "SSuprydp_button_option_text"=>"Click Here",
+            "SSuprydp_button_option_backg_color"=>"#4e0d61",
+            "SSuprydp_button_option_font"=>"Open Sans",
+            "SSuprydp_button_option_weight"=>"400",
+            "SSuprydp_button_option_size"=>"20",
+            "SSuprydp_button_option_align"=>"left",
+            "SSuprydp_button_option_color"=>"#fff",
+            "SSuprydp_content_option_text"=>"This is the Content Area. Put a description here of what you want to promote.",
+            "SSuprydp_content_option_font"=>"Open Sans",
+            "SSuprydp_content_option_weight"=>"800",
+            "SSuprydp_content_option_size"=>"25",
+            "SSuprydp_content_option_color"=>"#fff",
+            "SSuprydp_divider_option_color"=>"#1b7ccc",
+            "SSuprydp_action_option_text"=>"Click Here to View",
+            "SSuprydp_action_option_font"=>"Open Sans",
+            "SSuprydp_action_option_weight"=>"500",
+            "SSuprydp_action_option_size"=>"19",
+            "SSuprydp_action_option_color"=>"#fff",
+            "SSuprydp_action_option_url"=> "https://wpctapro.com/",
+            "SSuprydp_target_blank"=>"No",
+            "SSuprydp_nofollow"=>"No",
+            "SSuprydp_cta_position"=>"center",
             'sidebar_template' => 'sticky-cta',
             'line_separator_show' => 'yes',
             'line_separator_color' => '#fff',
@@ -108,10 +113,11 @@ class WP_Sticky_CTA_Data {
         $this->get_options();
 
         foreach ($this->sticky_data as $key => $value) {
+            // Set value to existing property or store in dynamic_properties
             if (property_exists($this, $key)) {
                 $this->$key = $value;
             } else {
-                // Store dynamic properties separately
+                // Store all other properties in dynamic_properties array
                 $this->dynamic_properties[$key] = $value;
             }
         }
@@ -129,11 +135,33 @@ class WP_Sticky_CTA_Data {
     public function __get($key) {
         if (property_exists($this, $key)) {
             return $this->$key;
-        } elseif (isset($this->dynamic_properties[$key])) {
+        }
+        
+        // Check for dynamic properties
+        if (isset($this->dynamic_properties[$key])) {
             return $this->dynamic_properties[$key];
+        }
+        
+        // Check in post_data for post-related information
+        if (isset($this->post_data[$key])) {
+            return $this->post_data[$key];
         }
 
         return null;
+    }
+    
+    /**
+     * Magic method to set dynamic properties
+     * @package sticky-sidebar
+     * @since   1.3.6
+     */
+    public function __set($key, $value) {
+        if (property_exists($this, $key)) {
+            $this->$key = $value;
+        } else {
+            // Store all other properties in dynamic_properties array
+            $this->dynamic_properties[$key] = $value;
+        }
     }
 
     /**
@@ -148,29 +176,17 @@ class WP_Sticky_CTA_Data {
 
         global $wpdb;
 
-        $options = $wpdb->get_results($wpdb->prepare(
-            "SELECT * FROM {$wpdb->prefix}sticky_cta_options WHERE sticky_cta_id = %d", 
-            $this->sticky_data->id
-        ));
-
+        $options = $wpdb->get_results($wpdb->prepare("SELECT * FROM {$wpdb->prefix}sticky_cta_options WHERE sticky_cta_id = %d", $this->sticky_data->id));
         foreach ($options as $option) {
-            $option_name = $option->option_name;
-            $option_value = maybe_unserialize($option->option_value);
-            
-            if (property_exists($this, $option_name)) {
-                $this->$option_name = $option_value;
-            } else {
-                $this->dynamic_properties[$option_name] = $option_value;
-                $this->sticky_data->$option_name = $option_value;
-            }
+            $this->sticky_data->{$option->option_name} = maybe_unserialize($option->option_value);
         }
     }
 
-    /**
+     /**
      * Get calculated CTR
      * @since 1.0.1
      */
-    public function get_ctr() {
+    function get_ctr() {
         if (absint($this->SSuprydp_impressions) > 0 && absint($this->SSuprydp_clicks) > 0) {
             return number_format((100 * $this->SSuprydp_clicks) / $this->SSuprydp_impressions, 2) . '%';
         }
@@ -185,12 +201,10 @@ class WP_Sticky_CTA_Data {
      */
     public function get_locations() {
         $get_location_types = wordpress_cta_pro_get_location_types();
+        
+        $locations = (array) ($this->locations ?: []);
 
-        $locations = isset($this->dynamic_properties['locations']) ? 
-                    $this->dynamic_properties['locations'] : 
-                    (isset($this->locations) ? $this->locations : array());
-
-        $this->locations = array_map(function($item) use($get_location_types) {
+        $processed_locations = array_map(function($item) use($get_location_types) {
             if (is_object($item)) {
                 $item = (array) $item;
             }
@@ -206,12 +220,13 @@ class WP_Sticky_CTA_Data {
             $item['label'] = @$get_location_types[$item['group']][$item['object']];
 
             if (!isset($item['values']) || !is_array($item['values'])) {
-                $item['values'] = array();
+                $item['values'] = [];
             }
 
             return $item;
-        }, (array) $locations);
-
+        }, $locations);
+        
+        $this->locations = $processed_locations;
         return $this->locations;
     }
 
@@ -222,12 +237,10 @@ class WP_Sticky_CTA_Data {
      */
     public function get_exclude_locations() {
         $get_location_types = wordpress_cta_pro_get_location_types();
+        
+        $exclude_locations = (array) ($this->exclude_locations ?: []);
 
-        $exclude_locations = isset($this->dynamic_properties['exclude_locations']) ? 
-                           $this->dynamic_properties['exclude_locations'] : 
-                           (isset($this->exclude_locations) ? $this->exclude_locations : array());
-
-        $this->exclude_locations = array_map(function($item) use($get_location_types) {
+        $processed_exclude_locations = array_map(function($item) use($get_location_types) {
             if (!isset($item['type'])) {
                 return $item;
             }
@@ -239,12 +252,13 @@ class WP_Sticky_CTA_Data {
             $item['label'] = @$get_location_types[$item['group']][$item['object']];
 
             if (!isset($item['values']) || !is_array($item['values'])) {
-                $item['values'] = array();
+                $item['values'] = [];
             }
 
             return $item;
-        }, (array) $exclude_locations);
-
+        }, $exclude_locations);
+        
+        $this->exclude_locations = $processed_exclude_locations;
         return $this->exclude_locations;
     }
 
@@ -254,8 +268,22 @@ class WP_Sticky_CTA_Data {
      * @return array Object as array.
      */
     public function to_array() {
-        $properties = get_object_vars($this);
-        unset($properties['dynamic_properties']);
-        return array_merge($properties, $this->dynamic_properties);
+        $vars = get_object_vars($this);
+        
+        // Add dynamic properties to the returned array
+        if (!empty($this->dynamic_properties)) {
+            $vars = array_merge($vars, $this->dynamic_properties);
+        }
+        
+        // Add post data to the returned array
+        if (!empty($this->post_data)) {
+            $vars = array_merge($vars, $this->post_data);
+        }
+        
+        // Remove internal tracking properties from the returned array
+        unset($vars['dynamic_properties']);
+        unset($vars['sticky_data']);
+        
+        return $vars;
     }
 }
--- easy-sticky-sidebar/views/default.php
+++ easy-sticky-sidebar/views/default.php
@@ -1,59 +1,107 @@
 <?php
 if (!defined('ABSPATH')) {
-	exit; // Exit if accessed directly 
+	exit; // Exit if accessed directly
 }
 
-if ('yes' == $ctacontent->collapse_on_page_load) {
-	array_push($cta_classes, 'shrink');
+// Initialize variables with fallback
+$button_color = $button_background_color = $content_color = $contents_background_color = $link_color = $links_text_background = '';
+$cta_classes = [];
+
+// Safely access and assign values
+if (!empty($ctacontent->SSuprydp_button_option_color)) {
+	$button_color = sanitize_hex_color($ctacontent->SSuprydp_button_option_color);
+}
+
+if (!empty($ctacontent->SSuprydp_button_option_backg_color)) {
+	$button_background_color = sanitize_hex_color($ctacontent->SSuprydp_button_option_backg_color);
+}
+
+if (!empty($ctacontent->SSuprydp_content_option_color)) {
+	$content_color = sanitize_hex_color($ctacontent->SSuprydp_content_option_color);
+}
+
+if (!empty($ctacontent->content_background_color)) {
+	$contents_background_color = sanitize_hex_color($ctacontent->content_background_color);
+}
+
+if (!empty($ctacontent->SSuprydp_action_option_color)) {
+	$link_color = sanitize_hex_color($ctacontent->SSuprydp_action_option_color);
+}
+
+if (!empty($ctacontent->link_text_background)) {
+	$links_text_background = sanitize_hex_color($ctacontent->link_text_background);
+}
+
+// Additional settings
+if (isset($ctacontent->collapse_on_page_load) && $ctacontent->collapse_on_page_load === 'yes') {
+	$cta_classes[] = 'shrink';
 }
 
 $cta_links_attrs = '';
 $tag = 'div';
-if ($ctacontent->SSuprydp_action_option_url) {
+
+if (!empty($ctacontent->SSuprydp_action_option_url)) {
 	$tag = 'a';
-	$cta_links_attrs = sprintf('href="%s"', esc_url_raw($ctacontent->SSuprydp_action_option_url));
-}
+	$cta_links_attrs = sprintf('href="%s"', esc_url($ctacontent->SSuprydp_action_option_url));
 
-if ($ctacontent->SSuprydp_target_blank == 'Yes') {
-	$cta_links_attrs .= ' target="_blank"';
-}
-
-if ($ctacontent->SSuprydp_nofollow == 'Yes') {
-	$cta_links_attrs .= ' rel="nofollow"';
+	if (!empty($ctacontent->SSuprydp_target_blank) && $ctacontent->SSuprydp_target_blank === 'Yes') {
+		$cta_links_attrs .= ' target="_blank"';
+	}
+	if (!empty($ctacontent->SSuprydp_nofollow) && $ctacontent->SSuprydp_nofollow === 'Yes') {
+		$cta_links_attrs .= ' rel="nofollow"';
+	}
 }
 
 ob_start(); ?>
 
-<div id="<?php echo 'easy-sticky-sidebar-' . esc_attr($ctacontent->id) ?>" class="<?php echo esc_attr(implode(' ', $cta_classes)) ?>" data-id="<?php echo esc_attr($ctacontent->id); ?>">
+<div id="<?php echo esc_attr('easy-sticky-sidebar-' . $ctacontent->id); ?>"
+    class="<?php echo esc_attr(implode(' ', $cta_classes)); ?>" data-id="<?php echo esc_attr($ctacontent->id); ?>">
 
-	<div class="sticky-sidebar-button">
-		<div><?php do_action('easy_sticky_sidebar_sticky_cta_button', $ctacontent); ?></div>
-		<?php
+    <div class="sticky-sidebar-button" style="background-color:<?php echo esc_attr($button_background_color); ?>">
+        <div style="color: <?php echo esc_attr($button_color); ?>;">
+            <?php do_action('easy_sticky_sidebar_sticky_cta_button', $ctacontent); ?>
+        </div>
+        <?php
 		if (function_exists('wordpress_cta_pro_get_close_button')) {
 			wordpress_cta_pro_get_close_button($ctacontent);
-		} ?>
-	</div>
+		}
+		?>
+    </div>
 
-	<<?php echo esc_html($tag) ?> class="sticky-sidebar-content sticky-sidebar-container" <?php echo $cta_links_attrs; ?>>
+    <<?php echo esc_html($tag); ?> class="sticky-sidebar-content sticky-sidebar-container"
+        <?php echo $cta_links_attrs; ?>>
+        <?php
+		$image = !empty($ctacontent->sticky_s_media) ? esc_url($ctacontent->sticky_s_media) : '';
 
-		<?php if ('yes' != $ctacontent->hide_cta_image) { ?>
-			<div class="sticky-sidebar-image"></div>
-		<?php } ?>
+		if (!empty($image) && $ctacontent->hide_cta_image !== 'yes') : ?>
+        <div class="sticky-sidebar-image" style="background-image: url('<?php echo $image; ?>');"></div>
+        <?php endif; ?>
 
-		<div class="sticky-sidebar-text sticky-content-inner"><?php echo do_shortcode(wp_kses_post($ctacontent->SSuprydp_content_option_text)); ?></div>
-		<?php
+        <div class="sticky-sidebar-text sticky-content-inner"
+            style="color: <?php echo esc_attr($content_color); ?>; background-color: <?php echo esc_attr($contents_background_color); ?>;">
+            <?php echo do_shortcode(wp_kses_post($ctacontent->SSuprydp_content_option_text)); ?>
+        </div>
 
-		if ($ctacontent->SSuprydp_action_option_url) {
-			if ($ctacontent->line_separator_show != 'no') {
+        <?php if (!empty($ctacontent->SSuprydp_action_option_url)) :
+			if ($ctacontent->line_separator_show !== 'no') {
 				echo '<hr>';
 			}
 
-			if ($ctacontent->hide_call_to_action != 'yes') {
-				printf('<div class="sticky-sidebar-call-to-action sticky-content-inner">%s</div>', wp_kses_post($ctacontent->SSuprydp_action_option_text));
-			}
-		} ?>
-	</<?php echo esc_html($tag) ?>>
-</div>
-<?php
+			if ($ctacontent->hide_call_to_action !== 'yes') {
+				$style = sprintf(
+					'color:%s; background-color:%s;',
+					esc_attr($link_color),
+					esc_attr($links_text_background)
+				);
 
-echo ob_get_clean();
+				printf(
+					'<div class="sticky-sidebar-call-to-action sticky-content-inner" style="%s">%s</div>',
+					$style,
+					wp_kses_post($ctacontent->SSuprydp_action_option_text)
+				);
+			}
+		endif; ?>
+    </<?php echo esc_html($tag); ?>>
+</div>
+
+<?php echo ob_get_clean(); ?>
